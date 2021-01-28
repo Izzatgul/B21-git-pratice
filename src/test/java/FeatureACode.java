ca1fcda76ddc55e367c71ed56e1c887d5578de33
Izzatgul <izzatgul0506@gmail.com>
2021-01-27 22:46:17 -0500

    another new commit featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -4,6 +4,7 @@ public class FeatureACode {
 
         System.out.println("Awesome work  for A started ");
         System.out.println("B21 is learning more git with review ");
+        System.out.println("Thanks Allah . Hope everything is good ");
 
 
 
